f8cda336f5978727679fcf427bfc50205c9a0b9e
lea-chefson <leac41@hotmail.com>
2023-05-03 02:48:34 +0700

    fixes homepage part 2

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 import "./Footer.css";
 // assets
-import linkedin from "../../assets/icons/linkedinLogo.png";
-import youtube from "../../assets/icons/youtubeLogo.png";
-import facebook from "../../assets/icons/facebookLogo.png";
-import instagram from "../../assets/icons/instagramLogo.png";
-import twitter from "../../assets/icons/twitterLogo.png";
-import reddit from "../../assets/icons/redditLogo.png";
-import discord from "../../assets/icons/discordLogo.png";
 import logo from "../../assets/images/logo.png";
-import Jump from "react-reveal/Jump";
 // 
 import {Link} from "react-router-dom";
 
@@ -17,11 +9,12 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer_top">
-        <div className="cs_container">
-          <div className="cs_row footer_row">
+        <div className="">
+          <div className=" footer_row">
             <div className="footer_left">
               <img src={logo} alt="Crypteco" />
             </div>
+            <div className="footer-text">
             <div className="footer_nav_col">
               <h4>Site Map</h4>
               <ul>
@@ -29,21 +22,19 @@ const Footer = () => {
                   <Link to="/">Home</Link>
                 </li>
                 <li>
-                  <Link to="/whoweare">Who we are</Link>
-                </li>
-                <li>
-                  <Link to="/business/gaiaswap">GAIASWAP </Link>
-                </li>
-                <li>
                   <Link to="/news-blog">News & Blogs</Link>
-                </li>
-                <li>
-                  <Link to="/business/ecoInvestments">Eco-Investments</Link>
                 </li>
                 <li>
                   <Link to="/contact">Contact</Link>
                 </li>
               </ul>
+            </div>
+            <div>
+            <h4>Contact</h4>
+            <p><strong>Ecostruct Holdings</strong></p>
+            <p>Craigmuir Chambers<br/> Road Town, Tortola<br/> VG 1110<br/> British Virgin Islands</p>
+            <a href="mailto:info@ecostructholdings.com">info@ecostructholdings.com</a>
+              </div>
             </div>
           </div>
         </div>
